f8231e892783cddd3fa1c8c30484427c097cfe96
cyrus chen <pschen@lcs.on.ca>
2023-05-23 11:35:59 -0400

    Initial Commit

---
+++ DaysSinceStatic/DaysSinceStaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSinceStaticApp.swift
+//  DaysSinceStatic
+//
+//  Created by Cyrus Chen on 23/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct DaysSinceStaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
